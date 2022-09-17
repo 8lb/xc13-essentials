e940d2059322467099760ce678f9014dd3831b2c
Albert Hon <honalbert@gmail.com>
2022-09-17 16:48:06 +1000

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Albert Hon on 17/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
